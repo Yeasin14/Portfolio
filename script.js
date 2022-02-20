8ae8134fcfc132b8048ab3a033be170d33dec95e
Yeasin14 <yeasinarafat14@gmail.com>
2022-02-20 11:31:53 +0600

    first commit

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ $(document).ready(function(){
     });
 
     // owl carousel script
+    
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
@@ -71,3 +72,48 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
+
+// send mail js
+
+function sendmail() {
+
+    var name = $('#Name').val();
+    var email = $('#Sender').val();
+    var subject = $('#Subject').val();
+    var message = $('#Message').val();
+
+    // var body = $('#body').val();
+
+    var Body = 'Name: ' + name + '<br>Email: ' + email + '<br>Subject: ' + subject + '<br>Message: ' + message;
+    //console.log(name, phone, email, message);
+
+    Email.send({
+        SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
+        To: 'mahfuzrahman038@gmail.com',
+        From: email,
+        Subject: "New message on Portfolio from " + name,
+        Body: Body
+    }).then(
+        message => {
+            //console.log (message);
+            if (message == 'OK') {
+                alert('Your mail has been sent. Thank you for contacting.');
+            }
+            else {
+                console.error(message);
+                alert('message could not send. ')
+
+            }
+
+        }
+    );
+    }
+
+
+
+
+
